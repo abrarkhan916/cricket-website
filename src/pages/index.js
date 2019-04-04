d29868e8fb9e0f3c5c1ec5402cd281e918f89daa
Abrar Khan <abrar.khan@hee.nhs.uk>
2019-04-04 16:11:51 +0100

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => 
+<body>
+<div><h1>Cricket World Cup 2019</h1></div>
+<p align="center">The ICC Cricket World Cup 2019 is coming to England and Wales this year. Many exciting matches to come so make sure you don't miss out on the tickets.</p>
+<button><a href="https://tickets.cricketworldcup.com/?tkhrq=36646346-1322-46b1-a597-b1c9c8de977e&tkhrp=49195949-959b-4f0b-99a5-b9959d952218&tkhrts=1533294815&tkhrc=tickethour&tkhre=cwc2019publicwindo&tkhrrt=Safetynet&tkhrh=3ec2785ba2e54122d8b751842f70a12e">Buy Tickets</a></button>
+<div class="ground"><img src="https://images.theabcdn.com/i/5184478/1500x375/c"></img></div>
+<div id="ball"></div>
+
+
+</body>
